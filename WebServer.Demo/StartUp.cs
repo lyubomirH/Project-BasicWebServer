93aca6924a809941c57c80fea6fb0c4b5725905f
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2026-02-12 12:44:36 +0200

    TO READ

--- WebServer.Demo/StartUp.cs
+++ WebServer.Demo/StartUp.cs
@@ -10,7 +10,7 @@ namespace WebServer.demo
             var server = new HttpServer(routes =>
             {
                 routes
-                .MapGet("/", new TextResponse("Hello from the server!"))
+                .MapGet("/", new TextResponse("Hello from the server!"))     // Единствено не работи
                 .MapGet("/HTML", new HtmlResponse("<h1>HTML response</h1>"))
                 .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"));
             });
