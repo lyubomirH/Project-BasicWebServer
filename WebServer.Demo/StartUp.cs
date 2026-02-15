cb76d87a99266f0e26a4e6262483f0b0a15215ff
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2026-02-15 21:21:06 +0200

    new comment about ERROR

--- WebServer.Demo/StartUp.cs
+++ WebServer.Demo/StartUp.cs
@@ -8,11 +8,11 @@ namespace WebServer.demo
         public static void Main()
         {
             var server = new HttpServer(routes =>
-            {                                    //Други проблеми може да дойдат от Extensions
+            {                                                                        //Други проблеми може да дойдат от Extensions
                 routes
-                .MapGet("/", new TextResponse("Hello from the server!"))    // Единствено не работи
-                .MapGet("/HTML", new HtmlResponse("<h1>HTML response</h1>"))
-                .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"));
+                .MapGet("/", new TextResponse("Hello from the server!"))             // Проблем при търсене на един и същ URL Програмата гърми
+                .MapGet("/HTML", new HtmlResponse("<h1>HTML response</h1>"))         //
+                .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"));  //
             });
             server.Start();
         }
