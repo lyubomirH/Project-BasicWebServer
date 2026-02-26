5069b66234336392eae396252de8c81d2a25f7a2
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2026-02-26 10:20:29 +0200

    new cookie class

---
+++ WebServer.Server/HTTP/CookieCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServer.Server.HTTP
+{
+    public class CookieCollection : IEnumerable<Cookie>
+    {
+        private readonly Dictionary<string, Cookie> cookies;
+
+        public CookieCollection()
+        {
+            cookies = new Dictionary<string, Cookie>();
+        }
+
+        public string this[string name]
+        {
+            
+        }
+        public void Add(string name, string value)
+        {
+            cookies[name] = new Cookie(name, value);
+        }
+
+        public bool
+        public IEnumerator<Cookie> GetEnumerator()
+        {
+            return cookies.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
